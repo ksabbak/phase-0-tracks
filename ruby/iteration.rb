aa8336926893ca19d349eb89d7922fbc1e30ee88
K. Sabbak <ksabbak@gmail.com>
2017-02-21 14:08:00 -0600

    Finish release 2 of 5.3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+#RELEASE 1
+
 neighborhoods = ["wicker park", "andersonville", "lake view", "lincoln park", "the loop"]
 bars = {wicker_park: "handlebar", lincoln_park: "blues", andersonville: "hopleaf", lake_view: "tavern", the_loop: "bar louie"}
 
@@ -14,6 +16,7 @@ bars.each do |neighborhood, bar|
 	puts "A bar in #{neighborhood.to_s.sub(/[_]/, " ")} is #{bar}"
 end
 
+#Capitalize each word
 p neighborhoods
 neighborhoods.map! do |neighborhood| 
 	words = neighborhood.split
@@ -23,3 +26,42 @@ neighborhoods.map! do |neighborhood|
 	neighborhood = words.join(" ")
 end
 p neighborhoods
+
+
+#RELEASE 2
+
+#1
+# neighborhoods.delete_if do |neighborhood|
+# 	neighborhood.include? " "
+# end
+# p neighborhoods
+
+# bars.delete_if do |neighborhood, bar|
+# 	bar.length > 6
+# end
+# p bars
+
+#2 - SHOULD RETURN SAME AS ABOVE
+# neighborhoods.keep_if do |neighborhood|
+# 	!neighborhood.include? " "
+# end
+# p neighborhoods
+
+# bars.keep_if do |neighborhood, bar|
+# 	bar.length <= 6
+# end
+# p bars
+
+#3
+new_neighborhoods = neighborhoods.drop_while do |neighborhood|
+	neighborhood.include? " "
+end
+p new_neighborhoods
+
+bars.keep_if do |neighborhood, bar|
+	bar.length <= 6
+	if bar.length <= 6
+		break
+	end
+end
+p bars
