0e034e6fea9d9a4a5a13eec242f73db53f687fd6
K. Sabbak <ksabbak@gmail.com>
2017-02-20 21:47:20 -0600

    Solve all problems in method_detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -27,18 +27,22 @@
 " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
+"The case of the disappearing last letter".chomp("r")
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter"[1..-1]
+"The mystery of the missing first letter".slice(1..-1)
+"The mystery of the missing first letter".sub(/[T]/, "")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".codepoints[0].to_s  #Okay obviously this is overkill, but it was my first guess.
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
