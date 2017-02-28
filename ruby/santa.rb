f84c7ca9af6b594231c954ba8c0c8d0f9bd22134
K. Sabbak <ksabbak@gmail.com>
2017-02-28 15:56:33 -0600

    Add a few new methods as well as getter and setter methods for the attributes.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,28 +1,23 @@
 class Santa
 
+	#attr_reader :gender, :ethnicity, :age, :reindeer_ranking
+
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		change_age
-		change_reindeer_ranking
+		@age = 0
+		@reindeer_ranking = [ "Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen" ]
 		puts "Initializing Santa instance with gender: #{@gender} and ethnicity: #{ethnicity}"	
 
 	end
-	
-	def change_reindeer_ranking(reindeer_ranking = [ "Rudolph", 
-												  "Dasher", 
-												  "Dancer", 
-												  "Prancer", 
-												  "Vixen", 
-												  "Comet", 
-												  "Cupid", 
-												  "Donner", 
-												  "Blitzen" ] )
-		@reindeer_ranking = reindeer_ranking
+
+	def celebrate_birthday
+		@age += 1
 	end
 
-	def change_age(age = 0)
-		@age = age
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer))
+		@reindeer_ranking << reindeer
 	end
 
 	#Takes no parameters
@@ -36,6 +31,31 @@ class Santa
 	#Returns nil
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}"
+	end
+
+
+	##SETTER METHODS
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	##GETTER METHODS
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	#I know these were not listed, but lbr here, if we have to change these things
+	#there should be some sort of way to check them???
+	def gender
+		@gender
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
 	end
 
 end
@@ -53,4 +73,14 @@ santa_attributes = [["agender", "black"],
 
 santa_attributes.each { |attributes| santas << Santa.new(attributes[0], attributes[1]) }
 
+p "This is Santa 7: #{santas[6].gender} #{santas[6].ethnicity} #{santas[6].age} #{santas[6].reindeer_ranking}"
 
+p "Santa 3 identifies as #{santas[2].gender}"
+santas[2].gender = "nonbinary"
+p "Santa 3 now identifies as #{santas[2].gender}"
+
+santas[0].get_mad_at("Rudolph")
+p santas[0].reindeer_ranking
+
+p santas[4].celebrate_birthday
+p santas[4].age
