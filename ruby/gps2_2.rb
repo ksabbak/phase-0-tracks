3cef7927c47f6f6c7f813262ae8b9add4021ddea
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-01 17:29:55 -0600

    Created pseudocode and empty methods

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,45 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # turn the string into an array (using split)
+  # set default quantity - default quantity is 1
+  # print the list to the console (call the print method)
+# output: hash where keys = food and values = quantity
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: add item name to list as key 
+# 		 and quantity as value (set value to one if not provided)
+# output: Maybe the updated hash? Not 100% needed.
+
+# Method to remove an item from the list
+# input: list, item name
+# steps: delete the key from the hash
+# output: Maybe the updated hash? 
+
+# Method to update the quantity of an item
+# input: list, item name, new quantity
+# steps: update the corresponding value to the item key
+# output: Maybe the updated hash?
+
+# Method to print a list and make it look pretty
+# input: list
+# steps: iterate through the list and update a string, it'll be nice.
+# output: the string
+
+def create_list(list_string)
+	
+end
+
+def add_item(list, item, value)
+
+end
+
+def remove_item(list, item)
+
+end
+
+def print_list(list)
+
+end
+
