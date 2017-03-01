eb0bc557266c3d9778dd0441ef3a327ba8821f49
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-01 10:00:25 -0600

    Make shout more musical

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,47 @@
+# module Shout
+# 	#Code coming soon
+# 	def self.yell_angrily(words)
+# 		words.upcase + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		"OMG " + words.upcase + "!!! YAY!!!"
+# 	end
+
+# end
+
+# p Shout.yell_happily("Chaise lounge")
+# p Shout.yell_angrily("I forgot to ask my roommate about the lights")
+
 module Shout
 	#Code coming soon
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words.upcase + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
+	def yell_happily(words)
 		"OMG " + words.upcase + "!!! YAY!!!"
 	end
 
 end
 
-p Shout.yell_happily("Chaise lounge")
-p Shout.yell_angrily("I forgot to ask my roommate about the lights")
+class IsleyBrothers
+	include Shout
+
+end
+
+class TearsForFears
+	include Shout
+
+end
+
+isley_brothers = IsleyBrothers.new
+puts isley_brothers.yell_happily("You know you make me want to shout")
+puts isley_brothers.yell_angrily("Kick my heals up and shout")
+
+tears_for_fears = TearsForFears.new
+puts tears_for_fears.yell_happily("Shout shout let it all out")
+puts tears_for_fears.yell_angrily("These are the things I can do without")
+
+
+
