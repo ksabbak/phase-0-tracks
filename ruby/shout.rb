34a00b89bd831c2f38ef4c836e6ca4c5acafc972
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-01 09:48:31 -0600

    Add yell happily method and driver code for testing

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -3,4 +3,12 @@ module Shout
 	def self.yell_angrily(words)
 		words.upcase + "!!!" + " :("
 	end
+
+	def self.yell_happily(words)
+		"OMG " + words.upcase + "!!! YAY!!!"
+	end
+
 end
+
+p Shout.yell_happily("Chaise lounge")
+p Shout.yell_angrily("I forgot to ask my roommate about the lights")
