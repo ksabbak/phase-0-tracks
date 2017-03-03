d4b157df1a3677eea6f1fd07054479764e5d65bf
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-03 14:59:02 -0600

    Create game pseudocode and test file

---
+++ ruby/hangman/game.rb
@@ -0,0 +1,33 @@
+THIS IS BASICALLY HANGMAN.
+
+CLASS PSEUDOCODE:
+
+Class vars: gameboard, original word, wrong guesses
+
+CREATE THE GAMEBOARD
+1. takes: The user input (string)
+2. gets the length of that input
+2.5 gets the length of the input without spaces
+3. Returns a string the same length of the user input with
+	non letter characters still there, everything else
+	replaced with _ - this is the GAMEBOARD
+
+PROCESS THE GUESS
+1. Takes in the letter
+2. If longer than 1 char, returns error
+3. If exists in original word, update GAMEBOARD
+	RETURN TRUE
+4. if not, increment wrong guesses by one
+	RETURN FALSE
+
+
+UPDATE GAMEBOARD
+1. Takes the letter
+2. Replaces all instances of _ with the letter.
+3. Updates the GAMEBOARD
+4. Returns whatever it wants, that doesn't matter.
+
+FINAL MESSAGE
+1. takes nothing
+2. if GAMEBOARD doesn't have _s then print something nice
+3. If GAMEBOARD does have _s then print something rude
