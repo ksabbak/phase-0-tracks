5688eaddf93022e72ae303e78e63ec0f4562cf0f
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-03 15:48:56 -0600

    Write tests that are sad because there are no methods in the actual class

--- ruby/hangman/game_spec.rb
+++ ruby/hangman/game_spec.rb
@@ -1,7 +1,22 @@
 require_relative 'game'
 
 describe Game do 
+	let(:game) {Game.new("Hello World!")}
 
+	it "Creates the board on initialization" do
+		expect(game.gameboard).to be ""
+	end
 	
-	
+	it "Checks user input guess against the word they're guessing" do
+		expeect(game.process_guess("e")).to be true
+	end
+
+	it "Updates the gameboard with the guess" do
+		game.update_gameboard("e")
+		expect(game.gameboard).to eq "_ e _ _ _  _ _ _ _ _!"
+	end
+
+	it "Returns good or bad message depending on win/loss" do
+		expect(game.final_message).to eq "Bad message" #THIS WILL CHANGE WHEN THE CODE IS UPDATED
+	end
 end
