614ed4f6f15b5a43bbb6b49d8bd212f5f4f986b9
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-06 21:43:33 -0600

    Finish refactoring.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -24,8 +24,10 @@ class VirusPredictor
   #Calls predicted_deaths - so the print statement from that gets printed
   #Calls speed_of_spread and returns whatever speed_of_spread returns 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    number_of_deaths = predicted_deaths
+    speed = speed_of_spread
+
+    "#{@state} will lose #{number_of_deaths} people in this outbreak and will spread across the state in #{speed} months.\n\n"
   end
 
   private
@@ -33,46 +35,47 @@ class VirusPredictor
   # Calculates death based on population density
   # Prints the string
   # Returns nil
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
+    
+    death_calc_percentage = 0
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    if @population_density >= 200
+      death_calc_percentage = 0.4
+    elsif @population_density >= 150
+      death_calc_percentage = 0.3
+    elsif @population_density >= 100
+      death_calc_percentage = 0.2
+    elsif @population_density >= 50
+      death_calc_percentage = 0.1
+    else
+      death_calc_percentage = 0.05
+    end
+    
+    number_of_deaths = (@population * death_calc_percentage).floor
+    
+    
 
   end
 
   # Calculates the speed based on population density 
   # prints a string
   # returns nil
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread   #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
@@ -102,7 +105,7 @@ end
 
 STATE_DATA.each do |state, data|
   modle = VirusPredictor.new(state, data[:population_density], data[:population])
-  modle.virus_effects
+  puts modle.virus_effects
 end
 
 
