3e29145ec00c712578c0d5eedfeba3501cf8aa7c
K Sabbak <ksabbak@Hogwarts-2.local>
2017-03-06 21:10:56 -0600

    Add comments explaining the code and loop the driver code to call all fifty states

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,18 +4,25 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# It retrieves the data from the "state_data.rb" file 
+# The relative part means it's from the same folder (and file path)
+# require sans relative needs the file path in order to work
 require_relative 'state_data'
 
 class VirusPredictor
 
+  # Every time we create a new VirusPredictor instance, the initializer gets called
+  # Gets called every time VirusPredictor.new is called
+  # Sets the initial values for the instance variables @state, @population and 
+  # @population_density
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #Calls predicted_deaths - so the print statement from that gets printed
+  #Calls speed_of_spread and returns whatever speed_of_spread returns 
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
@@ -23,6 +30,9 @@ class VirusPredictor
 
   private
 
+  # Calculates death based on population density
+  # Prints the string
+  # Returns nil
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +51,9 @@ class VirusPredictor
 
   end
 
+  # Calculates the speed based on population density 
+  # prints a string
+  # returns nil
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -70,17 +83,27 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+# Use a loop
+# Iterate through each state
+# to create a new VirusPredictor for each
+# Call virus effects
+
+STATE_DATA.each do |state, data|
+  modle = VirusPredictor.new(state, data[:population_density], data[:population])
+  modle.virus_effects
+end
 
 
 #=======================================================================
