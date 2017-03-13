bd9103a78e3afcdb2e36333db7203dad99d2465a
K. Sabbak <ksabbak@gmail.com>
2017-03-12 22:56:03 -0500

    Add pseudocode for a function that takes an int and creates an array of strings with the int number of items in that array.

--- js/algos.js
+++ js/algos.js
@@ -30,18 +30,6 @@ function getLongest(phrases){
 }
 
 /**
-In algos.js, write a function that takes two objects and checks to see if the objects 
-share at least one key-value pair. (You'll keep adding to this file, so we suggest 
-grouping functions at the top and testing at the bottom of the file.) 
-If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, 
-the function would return true, because at least one key-value pair matches between the 
-two objects. If no pairs match (and keep in mind that the two objects may not even have 
-any of the same keys), the function should return false. To make your life easier, 
-don't worry about whether a property is a string ('age') or an identifier name (age). 
-Those can be considered equivalent. Again, try to reason through the problem using the 
-basics you've already learned, rather than looking up slick search functions that will 
-do the job for you. We'd rather see you write code that you actually understand!
-
 Take: two objects
 Get the keys for all of object1. 
 Iterate through all the keys
@@ -61,6 +49,35 @@ function checkPairMatch(object1, object2){
 	}
 	return false;
 }
+
+/**
+Write a function that takes an integer for length, and builds and returns an array of 
+strings of the given length. So if we ran your function with an argument of 3, we would 
+get an array of 3 random words back (the words don't have to be actual sensical English 
+words -- "nnnnfph" totally counts). The words should be of randomly varying length, with 
+a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating 
+a random number, that you'll have to look up, but the solution on how to do so is 
+relatively straightforward.)
+
+Takes: length (int)
+create an alphabet var
+create a words array
+do the following the length number of times:
+	create a new string var
+	for a random number of times between 1 and 10
+		add a random char from alphabet to the string var
+	add the string var to the words array.
+
+RETURN: the words array 
+**/
+
+
+
+
+
+
+
+//DRIVER CODE HERE: 
 
 console.log(getLongest(["a", "b", "abc", "xyz"]));
 
