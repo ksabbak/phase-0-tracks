3efce75be1f6dc67d07f6803994e6a2267882d4a
K. Sabbak <ksabbak@gmail.com>
2017-03-20 10:29:34 -0500

    Add delete feature, clean up other parts of code.

--- databases/terminallygrateful/gratitude.rb
+++ databases/terminallygrateful/gratitude.rb
@@ -61,11 +61,14 @@ class Gratitude_Journal
 		
 		if range == "all"
 			entries = @db.execute("SELECT * FROM gratitude")
+		elsif range.include? "."
+			date_array = parse_date(range)
+			entries = @db.execute("SELECT * FROM gratitude WHERE month = #{date_array[0]} AND day = #{date_array[1]} AND year = #{date_array[2]}")
 		else
-			entries = @db.execute("SELECT ")
+			entries = @db.execute("SELECT * FROM gratitude WHERE year = #{range}")
 		end
 
-		sorted_entries = entries.sort { |a, b| [a["year"], a["month"], a["day"]] <=> [b["year"], b["month"], b["day"]] }
+		sorted_entries = sort_entries(entries)
 
 		if sorted_entries.length == 0
 			puts "Sorry, there are no entries for that range."
@@ -79,6 +82,60 @@ class Gratitude_Journal
 			ENTRY
 			end
 		end
+	end
+
+	def print_entries_with_id(range)
+		
+		if range == "all"
+			entries = @db.execute("SELECT * FROM gratitude")
+		elsif range.include? "."
+			date_array = parse_date(range)
+			entries = @db.execute("SELECT * FROM gratitude WHERE month = #{date_array[0]} AND day = #{date_array[1]} AND year = #{date_array[2]}")
+		else
+			entries = @db.execute("SELECT * FROM gratitude WHERE year = #{range}")
+		end
+
+		sorted_entries = sort_entries(entries)
+
+		if sorted_entries.length == 0
+			puts "Sorry, there are no entries for that range."
+		else
+			sorted_entries.each do |entry|
+				puts <<-ENTRY
+	#{entry['id']}	#{get_month(entry['month'])} #{entry['day']}, #{entry['year']}
+	Mood: #{entry['mood']}
+	You're grateful for: #{entry['gratitude']}
+
+			ENTRY
+			end
+		end
+	end
+
+	def sort_entries(entries)
+		entries.sort { |a, b| [a["year"], a["month"], a["day"]] <=> [b["year"], b["month"], b["day"]] }
+	end
+
+	def how_many_entries(date_string)
+		date_array = parse_date(date_string)
+		entries = @db.execute("SELECT * FROM gratitude WHERE month = #{date_array[0]} AND day = #{date_array[1]} AND year = #{date_array[2]}")
+		entries.length
+	end
+
+	def delete_entry(entry)
+		if entry.include? "."
+			date_array = parse_date(entry)
+			@db.execute("DELETE FROM gratitude WHERE month = #{date_array[0]} AND day = #{date_array[1]} AND year = #{date_array[2]}")
+		else
+			@db.execute("DELETE FROM gratitude WHERE id = #{entry}")
+		end
+	end
+
+	def parse_date(date)
+		date_array = date.split(".")
+		date_array.map do |part|
+			part.to_i
+		end
+		date_array
 	end
 
 	private
@@ -121,13 +178,25 @@ class Gratitude_Journal
 		end
 end
 
-#DRIVER CODE
+def commands
+	<<-COMMANDS
+	Commands:
+		+read 		read your old entries 
+		+write		write a new entry
+		+delete		delete an old entry 
+		+quit 		quit
+	COMMANDS
+end
 
-journal = Gratitude_Journal.new
+
+
+#DRIVER CODE
 puts "Welcome to your gratitude journal."
-entry_complete = false
-until entry_complete 
-	puts "Type +read to read your old entries or +write to write a new one."
+journal = Gratitude_Journal.new
+puts commands
+
+loop do
+	puts "Please enter a command: " 
 	input = gets.chomp
 	if input == "+write"
 		journal.set_date
@@ -141,7 +210,6 @@ until entry_complete
 		change_input = gets.chomp
 		unless (change_input != "n") && ("yes yeah definitely affirmative".include? change_input)
 			journal.create_entry
-			entry_complete = true
 			puts "Great! The entry has been created"
 		end
 	elsif input == "+read"
@@ -150,14 +218,36 @@ until entry_complete
 		if (quantity_input != "n") && ("yes yeah definitely affirmative all".include? quantity_input)
 			quantity = "all"
 		else
+			puts "Please enter the year you want to see the entries from: "
+			quantity = gets.chomp
 		end
-
 		journal.print_entries(quantity)
-		entry_complete = true
+	elsif input == "+delete"
+		puts "Please enter the date of the entry you want to delete (MM.DD.YYYY)"
+		delete_input = gets.chomp
+			if delete_input.include? "."
+				if journal.how_many_entries(delete_input) == 0
+					puts "There are no entries for that date"
+				elsif journal.how_many_entries(delete_input) > 1
+					puts "There is more than one entry for that date."
+					journal.print_entries_with_id(delete_input)
+					puts "Please enter the number you see before the entry to delete"
+					delete_id = gets.chomp
+					journal.delete_entry(delete_id)
+					puts "Entry deleted"
+				else
+					journal.delete_entry(delete_input)
+					puts "Entry deleted"
+				end
+			else
+				puts "Invalid input"
+			end
+	elsif input == "+help"
+		puts commands
 	elsif input == "+quit"
-		entry_complete = true
+		break
 	else
-		puts "please enter a valid input"
+		puts "please enter a valid input, don't forget the +"
 	end
 end
 
